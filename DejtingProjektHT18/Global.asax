f5680a7cdee10caca64cddc3ec75a44b3915ce7f
Fistlegs <Vince1@live.se>
2019-01-06 12:33:14 +0100

    Add project files.

---
+++ DejtingProjektHT18/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DejtingProjektHT18.MvcApplication" Language="C#" %>
---
+++ DejtingProjektHT18/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DejtingProjektHT18.Startup))]
+namespace DejtingProjektHT18
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
